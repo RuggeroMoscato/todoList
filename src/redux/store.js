8d0f2ac436d1f2dbfc96441e310b56172f75569e
RuggeroMoscato <ruggeromoscato04@gmail.com>
2024-03-11 17:26:53 +0100

    commenti e domande 2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
 import TodosReducer from "./reducers";
-import { persistReducer, persistStore,FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import { persistReducer, persistStore, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 
 const reducer = combineReducers({
   tasks: TodosReducer,
@@ -23,6 +23,9 @@ export const store = configureStore({
   devTools: process.env.NODE_ENV !== "production",
 });
 export const persistor = persistStore(store);
+
+
+
 //cosa significa deprecato? sono le funzioni che venivano usate prima ma che sono diventate obsolete.
 //come inizializzare lo store? Bisogna andare in index.js e mettere tutta l'app dentro un <Provider>.
 //per chiudere il terminale più velocemente premere control c.
@@ -30,3 +33,9 @@ export const persistor = persistStore(store);
 // a cosa serve'map'? a mappare l'array per poi fare una ricerca.
 //come eliminare un elemento da un array? usa filter.
 //come aumentare lo spazio fra un elemento e l'altro? usa gap nel css.
+//cosa è redux persist? è una libreria che permette al sistema di non dimenticare lo state prima di aggiornarsi.
+//come mettere un elemento in un array? usa push.
+//cosa è nanoid? è una funzione che attribuisce un id casuale ad un elemento.
+//come importare i font? importa i font da google cercando 'fonts' e scaricando il font desiderato.
+//come utilizzare il redux persist? inizializzalo nello store come fatto qui e importa PersistGate in 'index.js'.
+//cosa è useEffect? useEffect è un modo per chiamare una funzione quando un elemento viene cambiato.
